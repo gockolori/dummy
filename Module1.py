c46a8ab40aa635a3f33d1a784b6bc0b2a979a2d4
Unknown <veronikainvienna@gmail.com>
2018-01-11 14:04:12 +0100

    added module 1

---
+++ Module1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 13:59:00 2018
+
+@author: veronika
+"""
+
+import numpy as np
+import os
+
+def define_numbers(start,end,nb):
+    return np.linspace(start,end, nb)
